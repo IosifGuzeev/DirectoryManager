41c55c7570a1f5f489e1de8b4c5a08be543687a6
IosifGuzeev <Iosif.Guzeev@perfectart.com>
2020-03-28 19:57:22 +0700

    Added help message

--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
     ConsoleManager::Start();
     DirectoryManager::Start();
+    ConsoleManager::Write("Type \'help\' to get a list of all supported comands.");
+
     return a.exec();
 }
