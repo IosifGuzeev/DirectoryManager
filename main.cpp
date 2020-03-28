181577d7c841b5d314c389c9ada21aa01b659c95
IosifGuzeev <Iosif.Guzeev@perfectart.com>
2020-03-28 19:06:10 +0700

    Deleted test in main

--- main.cpp
+++ main.cpp
@@ -7,17 +7,7 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
     ConsoleManager::Start();
     DirectoryManager::Start();
-
-    QVector<QString> paths = {
-        "C:\\test\\1.txt",
-        "C:\\test\\2.txt",
-        "C:\\test\\3.txt",
-        "C:\\test\\4.txt",
-        "C:\\test\\5.txt"
-    };
-    DirectoryManager::AddFiles(paths);
     return a.exec();
 }
